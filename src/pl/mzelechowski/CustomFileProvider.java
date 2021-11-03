9e25a2e0539be239404f1a7fb04b42fa0e8e3a8b
maciek zelek <mzelechowski79@gmail.com>
2021-11-03 21:26:37 +0100

    update

--- src/pl/mzelechowski/CustomFileProvider.java
+++ src/pl/mzelechowski/CustomFileProvider.java
@@ -2,10 +2,8 @@ package pl.mzelechowski;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomFileProvider {
@@ -14,9 +12,13 @@ public class CustomFileProvider {
     public CustomFileProvider(String path) {
         this.path = path;
     }
- /** czytanie pliku linia po linii za pomocą klasy BufferedReader */
+
+    /**
+     * czytanie pliku linia po linii za pomocą klasy BufferedReader
+     */
     public void readFileBasic(String fileName) {
-        BufferedReader bufferedReader;
+        /** BufferedReader to klada czytająca znak po zanku (char) z uwzględniem znaku końca linii (\n)*/
+        BufferedReader bufferedReader; //TWorze referencję do obiektu
         try {
             FileReader fileReader = new FileReader(path + fileName);
             bufferedReader = new BufferedReader(fileReader);
@@ -29,37 +31,59 @@ public class CustomFileProvider {
             e.printStackTrace();
         }
     }
-    /** tworzenie pustego pliku */
-    public void createFileUsingFileClass(String filename){
-        File file = new File(path+filename);
-        try{
-            if(file.createNewFile()){
-                System.out.println("file created: "+ file.getAbsolutePath());
+
+    /**
+     * tworzenie pustego pliku
+     */
+    public void createFileUsingFileClass(String filename) {
+        File file = new File(path + filename);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("file created: " + file.getAbsolutePath());
             } else {
                 System.out.println("File already exists or another error");
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    /** zapisywanie pliku z podanem wartości w fomie List<String> */
-    public void createFileFromCollection(String filename, List<String> input, OpenOption option){
+
+    /**
+     * zapisywanie pliku z podanem wartości w fomie List<String>
+     */
+    public void createFileFromCollection(String filename, List<String> input, OpenOption option) {
         /** klasa Paths to klasa pomocnicza do operacji na ścieżkach dostęu do pliku*/
-        Path path = Paths.get(this.path+filename);
-        try{
+        Path path = Paths.get(this.path + filename);
+        try {
 /**klasa Files od Javy8 to klasa pomocnicza dla klasy File. Pomaga w operacjach na plikach*/
-            if(Files.notExists(path)){
+            if (Files.notExists(path)) {
                 Files.createFile(path);
             }/** Metoda  statycznafile write przyjmuje  takie parametry: ścieżke do pliku typu path
              liste danych w formie kolekcji String list<String>
              format zapisu danych, wybralem UTF_9
              opcjonalnie: opcje zapisu pliku: dodatnie danych do pliku istn. albo zastąpienie plku*/
+            String text = "!!! Koniec zapisau z listy " + System.lineSeparator() + System.lineSeparator();
             Files.write(path, input, StandardCharsets.UTF_8, option);
-        }catch(IOException e){
+            Files.write(path, text.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            //Files.writeString(path,"Jakis string "+"\n", StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
+    public List<String> createDataFromFile(String fileName) {
+        BufferedReader bufferedReader;  //Tworze referencję do obiektu
+        List<String> output = new ArrayList<>();
+        try {
+            bufferedReader = new BufferedReader(new FileReader(path + fileName));
+            String line = bufferedReader.readLine(); // czytam pierwszą linię  w pliku
+            while(line != null) {
+                output.add(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); //drukuje stack błędu
+        }
+        return output;
+    }
 }
--- src/pl/mzelechowski/Main.java
+++ src/pl/mzelechowski/Main.java
@@ -1,5 +1,9 @@
 package pl.mzelechowski;
 
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +12,14 @@ public class Main {
 	CustomFileProvider customFileProvider = new CustomFileProvider(path);
 	customFileProvider.readFileBasic(fileName);
 	customFileProvider.createFileUsingFileClass("maciek.txt");
+//        List<String> names = Arrays.asList("Maciek", "Anna", "Piotrek", "Danusia");
+//	customFileProvider.createFileFromCollection("file3.txt", names, StandardOpenOption.APPEND);
+
+	List<String> lista;
+	lista = customFileProvider.createDataFromFile("file3.txt");
+        for (String s:lista) {
+            System.out.println(s);
+        }
 
     }
 }
