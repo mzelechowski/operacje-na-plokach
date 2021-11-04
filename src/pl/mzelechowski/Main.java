2e70ce63a7a5d0d2d126d4e5be948c7142340d44
maciek zelek <mzelechowski79@gmail.com>
2021-11-04 09:22:09 +0100

    update

--- src/pl/mzelechowski/Main.java
+++ src/pl/mzelechowski/Main.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String path="C:\\Mike\\operacje-na-plokach\\src\\storage\\";
-        String fileName="jakisplik.txt";
-	CustomFileProvider customFileProvider = new CustomFileProvider(path);
-	customFileProvider.readFileBasic(fileName);
-	customFileProvider.createFileUsingFileClass("maciek.txt");
+        String path = "C:\\Mike\\operacje-na-plokach\\src\\storage\\";
+        String fileName = "jakisplik.txt";
+        CustomFileProvider customFileProvider = new CustomFileProvider(path);
+        customFileProvider.readFileBasic(fileName);
+        customFileProvider.createFileUsingFileClass("maciek.txt");
 //        List<String> names = Arrays.asList("Maciek", "Anna", "Piotrek", "Danusia");
 //	customFileProvider.createFileFromCollection("file3.txt", names, StandardOpenOption.APPEND);
 
-	List<String> lista, lista2;
-	lista = customFileProvider.createDataFromFile("file3.txt");
-        for (String s:lista) {
+        List<String> lista, lista2;
+        lista = customFileProvider.createDataFromFile("file3.txt");
+        for (String s : lista) {
             System.out.println(s);
         }
 
@@ -25,12 +25,11 @@ public class Main {
         System.out.println(lista2);
 
         List<Computer> computers = customFileProvider.readStorage("komputery.txt");
-        for (Computer c:computers){
+        for (Computer c : computers) {
             System.out.println(c);
         }
         //System.out.println(computers);
         customFileProvider.getWorldFromFile("pantadeusz.txt", "dom");
-
         customFileProvider.getDataFromUrl("https://www.wp.pl", "<div>");
     }
 }
